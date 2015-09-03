c19d07232674a09dce889a3f991769d7e7ffe625
Serkin Alexander <serkin.alexander@gmail.com>
2015-09-03 18:04:58 +0300

    Add public folder

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+require __DIR__ . '/../autoload.php';
+
+$config = include __DIR__ . '/../app/config.php';
+$app = new Application($config);
+
+$request = Request::createFromGlobals();
+
+$response = $app->handle($request);
+
+$app->terminate($response);
